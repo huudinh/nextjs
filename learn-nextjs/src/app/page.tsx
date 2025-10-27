bca7fdfb00929d66e59cdf5ea51982fac0df53da
huudinh <dinhndvn@yahoo.com>
2025-10-27 09:09:57 +0700

    update

--- learn-nextjs/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Home() {
-  return (
-    <div className={styles.page}>
-          <h1>Welcome to Next.js</h1>
-    </div>
-  );
-}
